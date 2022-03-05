feacf0970a0229cfc5a82a49ba4603d703628501
cccorpuz <ccorpuz@unomaha.edu>
2022-03-04 23:30:41 -0600

    add button to report

--- background.js
+++ background.js
@@ -3,6 +3,10 @@
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color});
+    chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ url: "https://www.smartgensociety.org/reporting" });
 });
--- popup.js
+++ popup.js
@@ -1,22 +1,28 @@
 let changeColor = document.getElementById("changeColor");
+let button = document.getElementById("reportBtn1");
 
-chrome.storage.sync.get("color", ({color}) => {
-    changeColor.style.backgroundColor = color;
-})
-
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-
-    chrome.scripting.executeScript(
-        {
-        target: {tabId: tab.id},
-        function: setPageBackgroundColor,
-        }
-    );
+// Reporting button event listener
+button.addEventListener("click", function() {
+    chrome.tabs.create({ url: "https://www.smartgensociety.org/reporting" });
 });
 
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({color}) => {
-        document.body.style.backgroundColor = color;
-    });
-}
+// chrome.storage.sync.get("color", ({ color }) => {
+//     changeColor.style.backgroundColor = color;
+// })
+
+// changeColor.addEventListener("click", async() => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         function: setPageBackgroundColor,
+//     });
+// });
+
+
+
+// function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//         document.body.style.backgroundColor = color;
+//     });
+// }
