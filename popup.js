b8d46172bca1cefbf5a162a5ca4bf2ef367879d8
cccorpuz <ccorpuz@unomaha.edu>
2022-03-10 13:19:34 -0600

    add connections between screens

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 var reportButton = document.getElementById("reportBtn1");
+var reportButtonYes = document.getElementById("reportBtnY");
+var reportButtonNo = document.getElementById("reportBtnN");
 var teenHelpButton = document.getElementById("teenHelpBtn1");
 var parentHelpButton = document.getElementById("parentHelpBtn1");
 var moreHelpButton = document.getElementById("moreHelpBtn1");
+var backButton = document.getElementById("backBtn1")
+var additionalResourcesBody = document.getElementById("additionalResourcesBody");
+var resourceHeader = document.getElementById("resourceHeader");
+var reportHeader = document.getElementById("reportHeader");
+var interactiveReportingButton = document.getElementById("interactiveReportingBtn1");
 
 // Reporting button event listener
 reportButton.addEventListener("click", function() {
-    chrome.tabs.create({ url: "https://www.smartgensociety.org/reporting" })
+    chrome.tabs.create({ url: "https://www.smartgensociety.org/reporting" });
 });
 
 teenHelpButton.addEventListener("click", function() {
@@ -13,11 +20,52 @@ teenHelpButton.addEventListener("click", function() {
 });
 
 parentHelpButton.addEventListener("click", function() {
-    chrome.tabs.create({ url: "https://www.smartgensociety.org/resources/parents" })
+    chrome.tabs.create({ url: "https://www.smartgensociety.org/resources/parents" });
+});
+
+reportButtonYes.addEventListener("click", function() {
+
+});
+
+reportButtonNo.addEventListener("click", function() {
+
 });
 
 moreHelpButton.addEventListener("click", function() {
-    chrome.tabs.executeScript({
-        code: 'console.log("hello");'
-    });
-}, false);
+    reportButton.style.display = "none";
+    reportHeader.style.display = "none";
+    moreHelpButton.style.display = "none";
+    backButton.style.display = "initial";
+    additionalResourcesBody.style.display = "initial";
+    teenHelpButton.style.display = "initial";
+    parentHelpButton.style.display = "initial";
+    resourceHeader.style.display = "initial";
+    additionalResourcesBody.style.display = "initial";
+    // interactiveReportingButton.style.display = "initial";
+});
+
+backButton.addEventListener("click", function() {
+    reportButton.style.display = "initial";
+    reportHeader.style.display = "initial";
+    moreHelpButton.style.display = "initial";
+    backButton.style.display = "none";
+    additionalResourcesBody.style.display = "none";
+    teenHelpButton.style.display = "none";
+    parentHelpButton.style.display = "none";
+    resourceHeader.style.display = "none";
+    additionalResourcesBody.style.display = "none";
+    interactiveReportingButton.style.display = "none";
+});
+
+// interactiveReportingButton.addEventListener("click", function() {
+//     reportButton.style.display = "none";
+//     reportHeader.style.display = "none";
+//     moreHelpButton.style.display = "none";
+//     backButton.style.display = "none";
+//     additionalResourcesBody.style.display = "none";
+//     teenHelpButton.style.display = "none";
+//     parentHelpButton.style.display = "none";
+//     resourceHeader.style.display = "none";
+//     additionalResourcesBody.style.display = "none";
+//     interactiveReportingButton.style.display = "none";
+// });
